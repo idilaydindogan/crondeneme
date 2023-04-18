a8cd03d4ac2414de8c10f317c5f27021fa181bd1
idildogan <idilaydindogan@gmail.com>
2023-04-18 16:38:32 +0300

    vercel

--- app/app/page.js
+++ app/app/page.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>EMAIL GONDERIMI</h2>
+      <h2>WEATHER APP EMAIL GONDERIMI</h2>
     </main>
   )
 }
